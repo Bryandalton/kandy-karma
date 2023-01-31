2cc301fda7960d8b0f80f43e1292a8a058f93f0d
Katie <thompsonkatherine704@gmail.com>
2023-01-30 20:32:20 -0500

    created index.js server file

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
