e22744464db6705213bf8076f6475160350534ea
Katie <thompsonkatherine704@gmail.com>
2023-01-30 21:16:29 -0500

    added seed data

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -2,11 +2,17 @@ const sequelize = require("../config/connection");
 const { Candy } = require("../models");
 
 const candyData = require("./candyData.json");
+const surveyData = require("./surveyData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Candy.bulkCreate(candyData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Candy.bulkCreate(surveyData, {
     individualHooks: true,
     returning: true,
   });
