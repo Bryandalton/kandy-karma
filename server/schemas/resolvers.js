04bd84f19f2ac6f5f4418ebf2dda3c04a0294888
Hshuttle <huntershutle@gmail.com>
2023-02-01 20:57:50 -0500

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,17 +18,39 @@ const resolvers = {
   },
   //   mutations will get the ones we put in the type mutations, this will model the user ones form in class activities ; then we will do the update and
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+  //   login(): Auth
+  //  addUser(): Auth
+  //  reviewCandy(): User
+  //  addNewCandy(): User X
+  //  deleteCandy(): User X
+  //  updateUser(): User X
+  //  deleteUser(): User X
+    addUser: async (parent, args) => {
+      const newUser = await User.create(args);
+      return newUser;
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
+    updateUser: async (parent, args) => {
+      const upUser = await User.findOneAndUpdate(args);
+      return upUser;
+    },
+    deleteUser: async (parent, args) => {
+      const remUser = await User.remove(
+        { _id }
+      );
+      return remUser;
+    },
+    addNewCandy: async (parent, args) => {
+      const newCandy = await Candy.create(
         { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
         { new: true }
       );
-      return vote;
+      return newCandy;
+    },
+    deleteCandy: async (parent, args) => {
+      const remCandy = await Candy.remove(
+        { _id }
+      );
+      return remCandy;
     },
   },
 };
