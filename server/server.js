94e80caa8f242f92adc18c6147f5312aa2e24b0b
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-02-02 19:46:33 -0500

    push to heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
