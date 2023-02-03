f7305deffa3b9080b8f7ac748571b6c981edc6f3
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-02-02 19:40:40 -0500

    pushing to heroku

--- server/server.js
+++ server/server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
