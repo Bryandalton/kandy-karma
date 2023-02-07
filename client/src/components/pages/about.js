44e44d9e0ad55764960e1372bac53801805cfb86
Katie <thompsonkatherine704@gmail.com>
2023-02-06 21:58:57 -0500

    Updated Client survey and login page

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -2,38 +2,49 @@ import React from "react";
 
 export default function about() {
   return (
-    <section className="h-100 gradient-form" style={{"background-color": "#eee;"}}>
-        <div className="container py-5 h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 about">
-              <div className="card rounded-3 text-black">
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="p-md-5 mx-md-4">
-                      <div className="text-center">
-                        <h4 className="mt-1 mb-5 pb-1">Welcome to Kandy Karma!</h4>
-                      </div>
-                      <p>At Kandy Karma is is our goal to provide a product testing environment suitable for your company's needs!  Speak with one of our Product Testing Lead's to book a testing session for your candy product.</p>
+    <section
+      className="h-100 gradient-form"
+      style={{ "background-color": "#eee;" }}
+    >
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 about">
+            <div className="card rounded-3 text-black">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="p-md-5 mx-md-4">
+                    <div className="text-center">
+                      <h4 className="mt-1 mb-5 pb-1">
+                        Welcome to Kandy Karma!
+                      </h4>
                     </div>
+                    <p>
+                      At Kandy Karma it is our goal to provide a product testing
+                      environment suitable for your company's needs! Speak with
+                      one of our Product Testing Lead's to book a testing
+                      session for your candy product.
+                    </p>
                   </div>
-                  <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                    <div className="text-white px-3 py-5 p-md-5 mx-md-4">
-                      <h4 className="mb-4">Contact us</h4>
-                      <ul>
-                        <li>Phone: (526) 395-2762</li>
-                        <li>Email: Info@KandyKarma.com</li>
-                        <li>Open for Appointments Only:
-                          {"\n"}
-                            7412 Sleepy Hollow Ave.
-                            Reynoldsburg, OH 43068</li>
-                      </ul>
-                    </div>
+                </div>
+                <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+                  <div className="text-white px-3 py-5 p-md-5 mx-md-4">
+                    <h4 className="mb-4">Contact us</h4>
+                    <ul>
+                      <li>Phone: (526) 395-2762</li>
+                      <li>Email: Info@KandyKarma.com</li>
+                      <li>
+                        Open for Appointments Only:
+                        {"\n"}
+                        7412 Sleepy Hollow Ave. Reynoldsburg, OH 43068
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 }
--- client/src/components/pages/survey.js
+++ client/src/components/pages/survey.js
@@ -1,39 +1,127 @@
 import React from "react";
-import fruityCandy from '../../images/fruitycandy.jpg';
-import chocolateCandy from '../../images/chocolatecandy.jpg';
-import sourCandy from '../../images/sourcandy.jpg';
+import fruityCandy from "../../images/fruitycandy.jpg";
+import chocolateCandy from "../../images/chocolatecandy.jpg";
+import sourCandy from "../../images/sourcandy.jpg";
+import Dropdown from "react-bootstrap/Dropdown";
 export default function Survey() {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-sm">
-          <div className="card candy-cards" style={{ width: 18 + 'rem' }}>
-            <img
-              className="card-img-top"
-              src={fruityCandy}
-            />
-            <div className="card-body candy-card-body">
-              <h5 className="card-title candy-card-title">Fruity Candy</h5>
-              <p className="card-text">
-                Answer questions about our newest Fruity Candies!
-              </p>
-              <ul className="list-group list-group-flush candy-list-group">
-                <li className="list-group-item">Fruity Candy Option 1</li>
-                <li className="list-group-item">Fruity Candy Option 2</li>
-                <li className="list-group-item">Fruity Candy Option 3</li>
-              </ul>
-              <a href="#" className="btn btn-primary candy-btn">
+      <div className="col-lg">
+        <div className="card candy-cards" style={{ width: 18 + "rem" }}>
+          <img className="card-img-top" src={fruityCandy} />
+          <div className="card-body candy-card-body">
+            <h5 className="card-title candy-card-title">Candy Survey</h5>
+            <p className="card-text">
+              Rate our candies on Texture, Look, Smell, Taste, and Overall
+              quality! On a scale of 1-5, where 5 is the highest option.
+            </p>
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control candyName"
+                  placeholder="Enter Candy Name *"
+                  value=""
+                />
+              </div>
+            </form>
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Candy Type
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Fruity Candy</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Sour Candy</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Chocolate Candy</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <ul className="list-group list-group-flush candy-list-group">
+              <li className="list-group-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Texture
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li className="list-group-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Look
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li className="list-group-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Smell
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li className="list-group-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Taste
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+              <li className="list-group-item">
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Overall
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+                    <Dropdown.Item href="#/action-4">4</Dropdown.Item>
+                    <Dropdown.Item href="#/action-5">5</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+            </ul>
+            {/* <a href="#" className="btn btn-primary candy-btn">
                 Review Me!
-              </a>
-            </div>
+              </a> */}
           </div>
         </div>
-        <div className="col-sm">
-          <div className="card candy-cards" style={{width: 18 + 'rem'}}>
-            <img
-              className="card-img-top"
-              src={chocolateCandy}
-            />
+      </div>
+      {/* <div className="col-sm">
+          <div className="card candy-cards" style={{ width: 18 + "rem" }}>
+            <img className="card-img-top" src={chocolateCandy} />
             <div className="card-body candy-card-body">
               <h5 className="card-title candy-card-title">Chocolate Candy</h5>
               <p className="card-text">
@@ -50,13 +138,10 @@ export default function Survey() {
               </a>
             </div>
           </div>
-        </div>
-        <div className="col-sm">
-          <div className="card candy-cards" style={{width: 18 + 'rem'}}>
-            <img
-              className="card-img-top"
-              src={sourCandy}
-            />
+        </div> */}
+      {/* <div className="col-sm">
+          <div className="card candy-cards" style={{ width: 18 + "rem" }}>
+            <img className="card-img-top" src={sourCandy} />
             <div className="card-body candy-card-body">
               <h5 className="card-title candy-card-title">Sour Candy</h5>
               <p className="card-text">
@@ -73,8 +158,7 @@ export default function Survey() {
               </a>
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
     </div>
   );
 }
