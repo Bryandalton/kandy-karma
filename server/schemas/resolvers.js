94d6b99f4e5ab83aeb4960ca51e3dd19ecd5198e
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2023-02-06 22:05:45 -0500

    add users fix

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const resolvers = {
   // setup me Query to parallell MERN books assignment
   Query: {
     users: async () => {
-      return User.find()
+      return User.find();
     },
     me: async (parent, args, context) => {
       if (context.user) {
@@ -18,73 +18,77 @@ const resolvers = {
       const candyData = await Candy.find();
       return candyData;
     },
-    candy: async (parent, {candyName}) =>{
-      return Candy.findOne(candyName)
+    candy: async (parent, { candyName }) => {
+      return Candy.findOne(candyName);
     },
-    surveys: async (parent, {email}) => {
-      return  Survey.find()
+    surveys: async (parent, { email }) => {
+      return Survey.find();
     },
     survey: async (parent, { surveyId }) => {
-      return Survey.findOne({_id: surveyId})
-    }
+      return Survey.findOne({ _id: surveyId });
+    },
   },
-  //   mutations will get the ones we put in the type mutations, this will model the user ones form in class activities ; then we will do the update and
-  Mutation: {
-  //   login(): Auth
-  //  addUser(): Auth
-  //  reviewCandy(): User
-  //  addNewCandy(): User X
-  //  deleteCandy(): User X
-  //  updateUser(): User X
-  //  deleteUser(): User X
-    addUser: async (parent, {email, password}) => {
-      const newUser = await User.create({email, password});
-      const token = signToken(newUser);
 
-      return {token, newUser};
+  Mutation: {
+    addUser: async (parent, { email, password, username }) => {
+      const user = await User.create({ email, password, username });
+      const token = signToken(user);
+
+      return { token, user };
     },
 
-    login: async (parent, {email, password}) => {
-      const user = await User.findOne({email});
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
 
-      if(!user) {throw new AuthenticationError('No user with this email found')}
+      if (!user) {
+        throw new AuthenticationError("No user with this name found");
+      }
 
       const correctPw = await user.isCorrectPassword(password);
-       if (!correctPw) {throw new AuthenticationError('Incorrect Credentials')}
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect Credentials");
+      }
 
       const token = signToken(user);
 
-      return{token, user};
+      return { token, user };
     },
 
-    updateUser: async (parent, {email}) => {
+    updateUser: async (parent, { email }) => {
       const upUser = await User.findOneAndUpdate(email);
       return upUser;
     },
-    deleteUser: async (parent, {userId}) => {
+    deleteUser: async (parent, { userId }) => {
       const remUser = await User.remove(
         { _id: userId } //remove user were id === userId?
       );
       return remUser;
     },
-    addNewCandy: async (parent, {candyName}) => {
-      const newCandy = await Candy.create(
-        { _id },
-        { new: true }
-      );
+    addNewCandy: async (parent, { candy_name }) => {
+      const newCandy = await Candy.create({ candy_name });
       return newCandy;
     },
-    deleteCandy: async (parent, {_id}) => {
-      const remCandy = await Candy.remove(
-        { _id }
-      );
+    deleteCandy: async (parent, { _id }) => {
+      const remCandy = await Candy.remove({ _id });
       return remCandy;
     },
-    addSurvey: async (parent, {surveyData}) => {
-      const survey = await Survey.create({surveyData})
+    addSurvey: async (
+      parent,
+      { candy_name, username, type, texture, look, smell, taste, overall }
+    ) => {
+      const survey = await Survey.create({
+        candy_name,
+        username,
+        type,
+        texture,
+        look,
+        smell,
+        taste,
+        overall,
+      });
       //I'm sure this is missing something ask Jamie
-      return survey
-    }
+      return survey;
+    },
   },
 };
 
